ee9e11c800f6a393f5806088b518fa034e68d5a8
Fred Kim <104876440+Fredkim21@users.noreply.github.com>
2023-03-01 18:38:48 -0800

    adding server side

---
+++ Personal Projects/golf-improvement-app/server/server.js
@@ -0,0 +1,57 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// dotenv.config();
+const app = express();
+app.use(express.json());
+// app.use(helmet());
+// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+// app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+const PORT = 3000;
+
+// static files from the 'build' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// get requests for each route handler
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.status(404).send('This is not the page you\'re looking for...'));
+
+// express universal error handling 
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error has occured' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status.json(errorObj.message));
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
+
+module.exports = app;
